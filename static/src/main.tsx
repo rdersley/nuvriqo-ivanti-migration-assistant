d714815b3a1283d9e272c70ab7e0c651959e5a6b
rdersley <rdersley@yahoo.com>
2026-09-03 15:16:33 +0100

    Enable orchestration view for saved migration projects

--- static/src/main.tsx
+++ static/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './orchestrationWorkflowView';
+import './orchestrationSavedEvidenceView';
 import './workflowInstanceCompat';
 import './workflowSemanticCleanup';
 import './safeOrchestrationBuild';
