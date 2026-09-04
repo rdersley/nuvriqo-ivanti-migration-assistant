75b81bc98491a404107e857a3f0e5131d8802318
rdersley <rdersley@yahoo.com>
2026-09-04 12:18:00 +0100

    Mount Ivanti workflow XML importer

--- static/src/main.tsx
+++ static/src/main.tsx
@@ -7,11 +7,13 @@ import './workflowSemanticCleanup';
 import './safeOrchestrationBuild';
 import App from './App';
 import IvantiConnectionMount from './IvantiConnectionMount';
+import WorkflowXmlImportMount from './WorkflowXmlImportMount';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <IvantiConnectionMount />
+    <WorkflowXmlImportMount />
   </React.StrictMode>
 );
